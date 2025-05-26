a1118fe067ae2d859cd49155541d4d35e2ff38b1
cg8 <5712.cg8@gmail.com>
2025-05-26 13:43:43 +0800

    Add a static path here.

--- src/config/express.js
+++ src/config/express.js
@@ -10,6 +10,7 @@ const configureExpress = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(express.static(path.join(__dirname, '../../public')));
+  app.use(express.static(path.join(__dirname, '../../src')));
 };
 
 module.exports = configureExpress;
